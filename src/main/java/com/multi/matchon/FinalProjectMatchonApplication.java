5ed2ecc232d9833dc9de13e5e898fe0b29cde19a
hyomin-dev <choihyomin248@gmail.com>
2025-05-15 02:14:43 +0900

    start final project

---
+++ src/main/java/com/multi/matchon/FinalProjectMatchonApplication.java
@@ -0,0 +1,13 @@
+package com.multi.matchon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectMatchonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectMatchonApplication.class, args);
+    }
+
+}
