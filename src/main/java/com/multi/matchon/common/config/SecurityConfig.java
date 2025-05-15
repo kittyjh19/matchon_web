160b86e26a165cf3cdf9eb7efa5b14ab60d5a921
seongeunchoi <sugar45675@naver.com>
2025-05-15 17:40:20 +0900

    메인 페이지 로그인 커밋 (#3)
    Co-authored-by: choi <>

---
+++ src/main/java/com/multi/matchon/common/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.multi.matchon.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/login", "/css/**","/img/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/main")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll());
+
+        return http.build();
+    }
+}
