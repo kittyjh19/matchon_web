acff989310727d2fb20428441f50b4b6701f09a6
Juhee Hong <163083158+kittyjh19@users.noreply.github.com>
2025-05-22 00:09:34 +0900

    Feature/mypage juhee 대회 등록 구현 (#67)
    * 로그아웃 토큰삭제 수정 + 기관명 업데이트 로직
    
    * 프로필 업로드 수정 완료
    
    * 로그아웃 에러 처리 + 관리자용 페이지 1차 완료
    
    * 회원가입 수정
    
    * 로그아웃 토큰삭제 수정 + 기관명 업데이트 로직
    
    * 프로필 업로드 수정 완료
    
    * 로그아웃 에러 처리 + 관리자용 페이지 1차 완료
    
    * 회원가입 수정
    
    * 삭제?
    
    * Feature/community wnsgur23 (#46)
    
    * 게시판
    
    * 마이페이지 추가 입력기능들 완료
    
    * 대회 등록 구현
    
    ---------
    
    Co-authored-by: choi <>
    Co-authored-by: QokuTpiX1 <107039584+wnsgur2358@users.noreply.github.com>

---
+++ src/main/java/com/multi/matchon/event/dto/res/EventSummaryDto.java
@@ -0,0 +1,10 @@
+package com.multi.matchon.event.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class EventSummaryDto {
+    private String eventTitle;
+}
