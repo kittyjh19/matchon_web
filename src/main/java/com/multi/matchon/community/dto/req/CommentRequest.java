4b330c60a33ffbf053005914c21a9dc8a9c4ecf4
QokuTpiX1 <107039584+wnsgur2358@users.noreply.github.com>
2025-06-03 02:06:28 +0900

    Feature/community wnsgur23 (#204)
    * 게시판 기본틀
    
    * 게시판 기본틀
    
    * 게시판 기본틀
    
    * 게시판 기본틀
    
    * 게시판 기본틀
    
    * 게시판 기본틀
    
    * 푸터수정
    
    * 푸터수정
    
    * 카테고리 변경 fetch 로 수정
    작성자 memberName 으로 설정 및 표시
    
    * 게시판 수정 댓글 삭제 처리
    게시판 삭제(미완료)
    
    * 피드백 "일부" 개선(ui, 비동기처리, 작성취소등)
    
    * 게시글 삭제 처리
    
    * 게시글 삭제 처리
    
    * 게시글 삭제 수정 처리
    
    * 게시글 수정
    
    * 게시글 수정
    
    * 게시글 수정
    
    * 게시글 수정
    
    * 게시글 수정
    
    * 게시글 수정
    
    * 게시글 오류 수정
    
    * 신고기능
    
    * 신고기능
    
    * css 분류 및 수정
    
    * css 분류 및 수정
    
    * 파일 업로드 경로 수정
    
    * Remove AdminInitConfig.java from Git tracking and ignore it
    
    * .
    
    * 신고 처리 요청 >> 응답 (신고 피드백x)
    
    * 수정
    
    * Fix: footer.html 최신 챗봇 UI 코드로 복원 (커밋 2e03fd5 기준)
    
    * 수정
    
    * 커뮤니티 공지사항 작성권한 수정
    
    * css 수정
    
    * css 수정
    
    * 신고초리 일부 수정
    
    * 신고처리 피드백 일부 구현, 신고 게시글/댓글 링크 추가
    
    * 작성일표시 오류 테스트
    
    * CSS 수정
    
    * 회원 로그인 기능에 정지된 계정 처리 로직 추가
    
    * 회원 정지 로직 일부 수정
    
    * 상단고정 기능 추가
    
    * 코드정리
    
    * 게시글 내 댓글수 카운트 표시 및 기타 오류 수정
    
    * ui수정
    
    * css 수정
    
    * css 수정
    
    * css 수정
    
    * 회원 정지로직 템플릿에서 일부 수정
    
    * 회원 정지로직 템플릿에서 일부 수정
    
    * 신고처리 오류수정
    
    * 신고처리 오류수정
    
    * 신고처리 오류수정
    
    * 신고처리 오류수정

--- src/main/java/com/multi/matchon/community/dto/req/CommentRequest.java
+++ src/main/java/com/multi/matchon/community/dto/req/CommentRequest.java
@@ -1,6 +1,7 @@
 package com.multi.matchon.community.dto.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,6 +9,7 @@ import lombok.Setter;
 @Setter
 public class CommentRequest {
     @NotBlank(message = "댓글 내용을 입력해주세요.")
+    @Size(max = 500, message = "댓글은 최대 500자까지 입력할 수 있습니다.")
     private String content;
 }
 
